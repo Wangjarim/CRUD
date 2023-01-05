f61c24b528c3bb2b97b22f671585983ad993c56b
Wangjarim <gaizka053@gmail.com>
2023-01-05 11:32:24 +0700

    crud

--- function.php
+++ function.php
@@ -1,24 +1,24 @@
 <?php 
     $connect = mysqli_connect("localhost", "root", "", "data_pegawai");
 
-    function query($query){
+    function query($query) {
         global $connect;
         $result = mysqli_query($connect, $query);
         $row = [];
-        while ($row = mysqli_fetch_assoc($result)){
+        while ($row = mysqli_fetch_assoc($result)) {
             $rows[] = $row;
         }
         return $rows;
     }
 
-    function tambah($data){
+    function tambah($data) {
         global $connect;
 
         $nama = $data["nama"];
-        $jabatan = $data ["jabatan"];
-        $umur = $data ["umur"];
+        $jabatan = $data["jabatan"];
+        $umur = $data["umur"];
 
-        $query = "INSERT INTO pegawai VALUES ('', '$nama', '$jabatan', '$umur')";
+        $query = "INSERT INTO pegawai VALUES ('', '$nama' , '$jabatan', '$umur')";
         mysqli_query($connect, $query);
 
         return mysqli_affected_rows($connect);
@@ -27,17 +27,18 @@
     function ubah($data){
         global $connect;
 
-        $id = $data("id");
-        $nama = $data("nama");
-        $jabatan = $data("jabatan");
-        $umur = $data("umur");
+        $id = $data["id"];
+        $nama = $data["nama"];
+        $jabatan = $data["jabatan"];
+        $umur = $data["umur"];
+
 
         $query = "UPDATE pegawai SET
                     nama = '$nama',
-                    jabatan = '$jabatan',
-                    umur = '$umur'
+                    jabatan    = '$jabatan',
+                    umur       = '$umur'
 
-                    WHERE id = $id
+                    WHERE id = $id 
         ";
 
         mysqli_query($connect, $query);
@@ -48,17 +49,16 @@
     function hapus($id) {
         global $connect;
         mysqli_query($connect, "DELETE FROM pegawai WHERE id = $id");
-
+    
         return mysqli_affected_rows($connect);
     }
 
     function cari($keyword) {
-        $query = "SELECT * FROM pegawai
-                    WHERE
+        $query = "SELECT * FROM pegawai 
+                    WHERE 
                     nama LIKE '%$keyword%' OR
-                    jabatan LIKE '%$keyword%'
+                    alamat LIKE '%$keyword%'
                 ";
-        
         return query($query);
     }
 ?>
--- index.php
+++ index.php
@@ -36,42 +36,20 @@ if (isset($_POST["search"])) {
 </style>
 
 <body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-            <a class="navbar-brand ms-5" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Dropdown
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled">Disabled</a>
-                    </li>
+<nav>
+        <div class="wrapper">
+            <div class="logo"><a href=""></a>SIPEG</div>
+            <!-- membuat menu  -->
+            <!-- <div class="menu">
+                <ul>
+                    <li><a href="#profil">Profil</a></li>
+                    <li><a href="#pendidikan">Pendidikan</a></li>
+                    <li><a href="#keterampilan">Keterampilan</a></li>
+                    <li><a href="#hobi">Hobi</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                    
                 </ul>
-                <form class="d-flex me-5" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Search" name="keyword" aria-label="Search">
-                    <button class="btn btn-outline-success" name="search" type="submit">Search</button>
-                </form>
-            </div>
+            </div> -->
         </div>
     </nav>
 
